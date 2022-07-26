e66415175ca8a8bfb63b5a2987f406c7273a28f2
Torben Henke <torben.henke@nordakademie.de>
2022-07-26 15:24:01 +0200

    aaaa

--- webcomponent.js
+++ webcomponent.js
@@ -1,9 +1,41 @@
 (function () {
-    let tmpl = document.createElement('template');
 
-             // creates a <table> element and a <tbody> element
-      const tbl = document.createElement("table");
-      const tblBody = document.createElement("tbody");
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = <input type="button" value="Generate a table" onclick="generate_table()"></input>
+
+    function generate_table() {
+        // creates a <table> element and a <tbody> element
+         const tbl = document.createElement("table");
+         const tblBody = document.createElement("tbody");
+       
+         // creating all cells
+         for (let i = 0; i < 2; i++) {
+           // creates a table row
+           const row = document.createElement("tr");
+       
+           for (let j = 0; j < 2; j++) {
+             // Create a <td> element and a text node, make the text
+             // node the contents of the <td>, and put the <td> at
+             // the end of the table row
+             const cell = document.createElement("td");
+             const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+             cell.appendChild(cellText);
+             row.appendChild(cell);
+           }
+       
+           // add the row to the end of the table body
+           tblBody.appendChild(row);
+         }
+       
+         // put the <tbody> in the <table>
+         tbl.appendChild(tblBody);
+         // appends <table> into <body>
+         document.body.appendChild(tbl);
+         // sets the border attribute of tbl to '2'
+         tbl.setAttribute("border", "2");
+         
+       }
+
 
     class HelloWorld1 extends HTMLElement {
 
@@ -38,36 +70,6 @@
         onCustomWidgetBeforeUpdate(oChangedProperties) {
 
 
-
-        }
-
-        set rowCount(newRowCount) {
-
-            // creating all cells
-            for (let newRowCount = 0; newRowCount < 2; newRowCount++) {
-                // creates a table row
-                const row = document.createElement("tr");
-
-                for (let j = 0; j < 2; j++) {
-                    // Create a <td> element and a text node, make the text
-                    // node the contents of the <td>, and put the <td> at
-                    // the end of the table row
-                    const cell = document.createElement("td");
-                    const cellText = document.createTextNode(`cell in row ${newRowCount}, column ${j}`);
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
-                }
-
-                // add the row to the end of the table body
-                tblBody.appendChild(row);
-            }
-
-            // put the <tbody> in the <table>
-            tbl.appendChild(tblBody);
-            // appends <table> into <body>
-            document.body.appendChild(tbl);
-            // sets the border attribute of tbl to '2'
-            tbl.setAttribute("border", "2");
 
         }
 
