713e243591fdf3dda2c37a605bb39a2e3ff942c6
Torben Henke <torben.henke@nordakademie.de>
2022-07-26 15:55:35 +0200

    aaaaa

--- webcomponent.js
+++ webcomponent.js
@@ -1,40 +1,5 @@
 (function () {
 
-    (function generate_table() {
-        // creates a <table> element and a <tbody> element
-         const tbl = document.createElement("table");
-         const tblBody = document.createElement("tbody");
-       
-         // creating all cells
-         for (let i = 0; i < 2; i++) {
-           // creates a table row
-           const row = document.createElement("tr");
-       
-           for (let j = 0; j < 2; j++) {
-             // Create a <td> element and a text node, make the text
-             // node the contents of the <td>, and put the <td> at
-             // the end of the table row
-             const cell = document.createElement("td");
-             const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
-             cell.appendChild(cellText);
-             row.appendChild(cell);
-           }
-       
-           // add the row to the end of the table body
-           tblBody.appendChild(row);
-         }
-       
-         // put the <tbody> in the <table>
-         tbl.appendChild(tblBody);
-         // appends <table> into <body>
-         document.body.appendChild(tbl);
-         // sets the border attribute of tbl to '2'
-         tbl.setAttribute("border", "2");
-         
-       }).call(this);
-
-    
-
     class HelloWorld1 extends HTMLElement {
 
 
@@ -53,7 +18,37 @@
         connectedCallback() {
 
             this._firstConnection = true;
-            this.redraw();
+
+            const tbl = document.createElement("table");
+            const tblBody = document.createElement("tbody");
+          
+            // creating all cells
+            for (let i = 0; i < 2; i++) {
+              // creates a table row
+              const row = document.createElement("tr");
+          
+              for (let j = 0; j < 2; j++) {
+                // Create a <td> element and a text node, make the text
+                // node the contents of the <td>, and put the <td> at
+                // the end of the table row
+                const cell = document.createElement("td");
+                const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+              }
+          
+              // add the row to the end of the table body
+              tblBody.appendChild(row);
+            }
+          
+            // put the <tbody> in the <table>
+            tbl.appendChild(tblBody);
+            // appends <table> into <body>
+            document.body.appendChild(tbl);
+            // sets the border attribute of tbl to '2'
+            tbl.setAttribute("border", "2");
+            
+   
         }
 
         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -70,9 +65,7 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
         onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection) {
-                this.redraw();
-            }
+           
         }
 
         //When the custom widget is removed from the canvas or the analytic application is closed
