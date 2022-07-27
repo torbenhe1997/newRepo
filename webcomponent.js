c7550ed14dccfe92c29f0d31eb46c06fba502cc3
Torben Henke <torben.henke@nordakademie.de>
2022-07-27 12:04:03 +0200

    aaa
    ZZ

--- webcomponent.js
+++ webcomponent.js
@@ -3881,28 +3881,7 @@
             var ctx = this.shadowRoot.getElementById('chart_div');
             google.charts.load('current', {'packages': ['table']});
 
-            google.charts.setOnLoadCallback(function () {
-                drawTable();
-            });
-           
-
-            function drawTable() {
-                var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Name');
-                data.addColumn('number', 'Salary');
-                data.addColumn('boolean', 'Full Time Employee');
-                data.addRows([
-                  ['Mike',  {v: 10000, f: '$10,000'}, true],
-                  ['Jim',   {v:8000,   f: '$8,000'},  false],
-                  ['Alice', {v: 12500, f: '$12,500'}, true],
-                  ['Bob',   {v: 7000,  f: '$7,000'},  true]
-                ]);
-        
-              
-                var table = new google.visualization.Table(ctx);      
-                table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
-              }
-    
+      
           
        
 
