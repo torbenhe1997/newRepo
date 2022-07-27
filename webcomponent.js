87a7bfd1ca2cd1afe3690a1ec81bb25cc682ee03
Torben Henke <torben.henke@nordakademie.de>
2022-07-27 12:34:04 +0200

    aa

--- webcomponent.js
+++ webcomponent.js
@@ -3837,7 +3837,7 @@
 		`;
 
 
-        class HelloWorld1 extends HTMLElement {
+    class HelloWorld1 extends HTMLElement {
 
 
         constructor() {
@@ -3851,21 +3851,16 @@
                 this.dispatchEvent(event);
             });
             this._props = {};
+
         }
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback() {
 
-           
+
 
         }
 
-        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback() {
-
-        
-
-        }
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
         onCustomWidgetBeforeUpdate(oChangedProperties) {
@@ -3873,7 +3868,7 @@
 
         }
 
-       
+
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
         onCustomWidgetAfterUpdate(oChangedProperties) {
@@ -3881,27 +3876,49 @@
             var ctx = this.shadowRoot.getElementById('chart_div');
             google.charts.load('current', {'packages': ['table']});
 
-      
-          
-       
-
-        }
-
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy() {}
+            google.charts.setOnLoadCallback(function () {
+                drawTable();
+            });
 
 
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
-        /*
-        onCustomWidgetResize(width, height){
-        }
-        */
+            function drawTable() {
+        
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Name');
+                data.addColumn('number', 'Salary');
+                data.addColumn('boolean', 'Full Time Employee');
+                data.addRows([
+                    ['Mike', {
+                        v: 10000,
+                        f: '$10,000'
+                    }, true],
+                    ['Jim', {
+                        v: 8000,
+                        f: '$8,000'
+                    }, false],
+                    ['Alice', {
+                        v: 12500,
+                        f: '$12,500'
+                    }, true],
+                    ['Bob', {
+                        v: 7000,
+                        f: '$7,000'
+                    }, true]
+                ]);
 
-        redraw() {
 
-            
+                var table = new google.visualization.Table(ctx);
+                table.draw(data, {
+                    showRowNumber: true,
+                    width: '100%',
+                    height: '100%'
+                });
+            }
+
+
+
+
         }
     }
-    customElements.define('com-sap-sample-helloworld1',HelloWorld1);
+    customElements.define('com-sap-sample-helloworld1', HelloWorld1);
 })();
