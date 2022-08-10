18b6337bb99683d107a71f1cf740a3c3c291ed02
Torben Henke <torben.henke@nordakademie.de>
2022-08-10 11:01:53 +0200

    aa

--- webcomponent.js
+++ webcomponent.js
@@ -16883,7 +16883,7 @@
 
                         dpi: 300, // Set to 300 DPI
                         scale: 3, // Adjusts your resolution
-                        onrendered: function (canvas) {
+                         function (canvas) {
                             var img = canvas.toDataURL("image/jpeg", 1);
                             var doc = new jsPDF('L', 'px', [w, h]);
                             doc.addImage(img, 'JPEG', 0, 0, w, h);
